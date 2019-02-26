0cdc8388b0bf3cccb76323864857044ee374ac3c
alexguillon <cyclopedeveloppeur@gmail.com>
2019-02-26 17:24:46 +0000

    Finished filtering

---
+++ dist/js/object-creation.js
@@ -0,0 +1,43 @@
+        var paris = {};
+        paris.price = 39;
+        paris.child = 3;
+        paris.availability = false;
+        paris.breakfast = true;
+        paris.htmlName = document.getElementById("paris");
+
+        var londres = {};
+        londres.price = 42;
+        londres.child = 2;
+        londres.availability = true;
+        londres.breakfast = true;
+        londres.htmlName = document.getElementById("londres");
+
+        var madrid = {};
+        madrid.price = 51;
+        madrid.child = 6;
+        madrid.availability = true;
+        madrid.breakfast = false;
+        madrid.htmlName = document.getElementById("madrid");
+
+        var rome = {};
+        rome.price = 25;
+        rome.child = 0;
+        rome.availability = true;
+        rome.breakfast = false;
+        rome.htmlName = document.getElementById("rome");
+
+        var ny = {};
+        ny.price = 38;
+        ny.child = 2;
+        ny.availability = false;
+        ny.breakfast = true;
+        ny.htmlName = document.getElementById("ny");
+
+        var lecap = {};
+        lecap.price = 2;
+        lecap.child = 5;
+        lecap.availability = true;
+        lecap.breakfast = true;
+        lecap.htmlName = document.getElementById("lecap");
+
+        var trips = [paris, londres, madrid, rome, ny, lecap];
